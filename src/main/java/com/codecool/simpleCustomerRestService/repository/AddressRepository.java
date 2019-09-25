3729e426653af1d499334c85dd096fc76d5cb0bb
szymslow12 <szym.slow12@gmail.com>
2019-09-25 20:11:05 +0200

    remove final from isSequence, add Map data, add get/add methods

--- src/main/java/com/codecool/simpleCustomerRestService/repository/AddressRepository.java
+++ src/main/java/com/codecool/simpleCustomerRestService/repository/AddressRepository.java
@@ -1,7 +1,30 @@
 package com.codecool.simpleCustomerRestService.repository;
 
+import com.codecool.simpleCustomerRestService.domain.Address;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddressRepository {
 
-    private static final Long idSequence = 0L;
+    private static Long idSequence = 0L;
 
+    private static final Map<Long, Address> data = new HashMap<>();
+
+    public Address add(Address address) {
+        if (address == null) {
+            throw new IllegalArgumentException("Address cannot be null!");
+        }
+        address.setId(idSequence);
+        data.put(idSequence++, address);
+        return address;
+    }
+
+
+    public Address get(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("ID cannot be null!");
+        }
+        return data.get(id);
+    }
 }
