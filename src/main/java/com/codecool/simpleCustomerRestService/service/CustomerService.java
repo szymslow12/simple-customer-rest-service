fda6d3776dae8e99ec692a042061d398833ac180
szymslow12 <szym.slow12@gmail.com>
2019-09-25 20:21:15 +0200

    add CustomerService

---
+++ src/main/java/com/codecool/simpleCustomerRestService/service/CustomerService.java
@@ -0,0 +1,34 @@
+package com.codecool.simpleCustomerRestService.service;
+
+import com.codecool.simpleCustomerRestService.domain.Customer;
+import com.codecool.simpleCustomerRestService.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CustomerService {
+
+    private CustomerRepository customerRepository;
+    private AddressService addressService;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository, AddressService addressService) {
+        this.customerRepository = customerRepository;
+        this.addressService = addressService;
+    }
+
+
+    public Customer add(Customer customer) {
+        if (customer == null) {
+            throw new IllegalArgumentException("Customer cannot be null!");
+        }
+        addressService.add(customer.getAddress());
+        return customerRepository.add(customer);
+    }
+
+
+    public Customer get(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("ID cannot be null!");
+        }
+        return customerRepository.get(id);
+    }
+}
