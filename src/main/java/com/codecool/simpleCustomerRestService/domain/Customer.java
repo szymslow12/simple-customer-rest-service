67fc5b065600ffff633451d8db8b2890759ba0fd
szymslow12 <szym.slow12@gmail.com>
2019-09-25 19:59:08 +0200

    add Customer entity

---
+++ src/main/java/com/codecool/simpleCustomerRestService/domain/Customer.java
@@ -0,0 +1,67 @@
+package com.codecool.simpleCustomerRestService.domain;
+
+public class Customer {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private Address address;
+
+    public Customer(String firstName, String lastName, String phoneNumber, String email, Address address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
