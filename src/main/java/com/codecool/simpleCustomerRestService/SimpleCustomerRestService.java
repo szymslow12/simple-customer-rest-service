9a03549e6820129dbf1c9433f5404bd1a93ac720
szymslow12 <szym.slow12@gmail.com>
2019-09-25 19:53:43 +0200

    add SimpleCustomerRestService

---
+++ src/main/java/com/codecool/simpleCustomerRestService/SimpleCustomerRestService.java
@@ -0,0 +1,12 @@
+package com.codecool.simpleCustomerRestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCustomerRestService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCustomerRestService.class, args);
+    }
+}
